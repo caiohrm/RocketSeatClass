42683cc17c3b7373ce21a13d79d1bc8cc94a3e14
Caio Henrique Ribeiro Martins <caio-h-rm@hotmail.com>
2019-04-09 22:46:45 -0300

    alterado erro em cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const cors = require('cors');
-app.use(cors)
+app.use(cors())
 
 const path = require('path');
 const server = require('http').Server(app)
